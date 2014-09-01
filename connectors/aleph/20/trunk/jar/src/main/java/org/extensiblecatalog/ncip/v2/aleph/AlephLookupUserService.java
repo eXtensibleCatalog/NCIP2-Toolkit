4dc59d58630318473c61df9fbe423566f01f3742
Jiří Kozlovský <mail@jkozlovsky.cz>
2014-09-01 14:27:10 +0200

    Modified Sample XMLs (added some) & pointed LookupUserService to restdlf & enabled this service in toolkit.properties & modified front end testing page

--- connectors/aleph/20/trunk/jar/src/main/java/org/extensiblecatalog/ncip/v2/aleph/AlephLookupUserService.java
+++ connectors/aleph/20/trunk/jar/src/main/java/org/extensiblecatalog/ncip/v2/aleph/AlephLookupUserService.java
@@ -13,78 +13,76 @@ import java.io.IOException;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.log4j.Logger;
-import org.extensiblecatalog.ncip.v2.aleph.AlephXServices.AlephException;
-import org.extensiblecatalog.ncip.v2.aleph.AlephXServices.AlephRemoteServiceManager;
-import org.extensiblecatalog.ncip.v2.aleph.AlephXServices.user.AlephUser;
+import org.extensiblecatalog.ncip.v2.aleph.restdlf.AlephException;
+import org.extensiblecatalog.ncip.v2.aleph.restdlf.AlephRemoteServiceManager;
+import org.extensiblecatalog.ncip.v2.aleph.restdlf.user.AlephUser;
 import org.extensiblecatalog.ncip.v2.service.*;
 import org.xml.sax.SAXException;
 
 /**
- * This class implements the Lookup User service for the Dummy back-end connector. Basically this just
- * calls the DummyRemoteServiceManager to get hard-coded data (e.g. title, call #, etc.).
+ * This class implements the Lookup User service for the Dummy back-end connector. Basically this just calls the DummyRemoteServiceManager to get hard-coded data (e.g. title, call #, etc.).
  * <p/>
- * Note: If you're looking for a model of how to code your own ILS's NCIPService classes, do not
- * use this class as an example. See the NCIP toolkit Connector developer's documentation for guidance.
+ * Note: If you're looking for a model of how to code your own ILS's NCIPService classes, do not use this class as an example. See the NCIP toolkit Connector developer's documentation for guidance.
  */
 public class AlephLookupUserService implements LookupUserService {
-	
+
 	static Logger log = Logger.getLogger(AlephLookupUserService.class);
 
-    /**
-     * Handles a NCIP LookupUser service by returning hard-coded data.
-     *
-     * @param initData       the LookupUserInitiationData
-     * @param serviceManager provides access to remote services
-     * @return LookupUserResponseData
-     */
-    @Override
-    public LookupUserResponseData performService(LookupUserInitiationData initData,
-                                                 ServiceContext serviceContext,
-                                                 RemoteServiceManager serviceManager) {
+	/**
+	 * Handles a NCIP LookupUser service by returning hard-coded data.
+	 *
+	 * @param initData
+	 *            the LookupUserInitiationData
+	 * @param serviceManager
+	 *            provides access to remote services
+	 * @return LookupUserResponseData
+	 */
+	@Override
+	public LookupUserResponseData performService(LookupUserInitiationData initData, ServiceContext serviceContext, RemoteServiceManager serviceManager) {
+		//TODO: Think about forwarding password in encrypted format ({@link Version1AuthenticationDataFormatType.APPLICATION_AUTH_POLICY_XML})
 
-        final LookupUserResponseData responseData = new LookupUserResponseData();
-        AlephRemoteServiceManager alephRemoteServiceManager = (AlephRemoteServiceManager) serviceManager;
-        
-        String patronId = null;
-        String password = null;
-        
-        for (AuthenticationInput authInput : initData.getAuthenticationInputs()) {
-        	if (authInput.getAuthenticationInputType().getValue().equals("Username")) {
-        		patronId = authInput.getAuthenticationInputData();
-        	}
-        	if (authInput.getAuthenticationInputType().getValue().equals("Password")) {
-        		password = authInput.getAuthenticationInputData();
-        	}
-        }
-        
-        AlephUser user = null;
-        
-        try {
-        	String agencyId = "MZK"; // FIXME
-        	user = alephRemoteServiceManager.authenticateUser(agencyId, patronId, password);
-        } catch (Exception ex) {
-        	ex.printStackTrace();
-        }
+		final LookupUserResponseData responseData = new LookupUserResponseData();
+		AlephRemoteServiceManager alephRemoteServiceManager = (AlephRemoteServiceManager) serviceManager;
 
-        //AgencyId agencyId = new AgencyId(dummySvcMgr.getLibraryName());
+		String patronId = null;
+		String password = null;
 
-        // Echo back the same item id that came in
-        responseData.setUserId(initData.getUserId());
-        
+		for (AuthenticationInput authInput : initData.getAuthenticationInputs()) {
+			if (authInput.getAuthenticationInputType().getValue().equals("Username")) {
+				patronId = authInput.getAuthenticationInputData();
+			}
+			if (authInput.getAuthenticationInputType().getValue().equals("Password")) {
+				password = authInput.getAuthenticationInputData();
+			}
+		}
 
-        UserOptionalFields userOptionalFields = new UserOptionalFields();
+		AlephUser user = null;
 
-        if (user != null && initData.getNameInformationDesired()) {
-            PersonalNameInformation pni = new PersonalNameInformation();
-            pni.setUnstructuredPersonalUserName(user.getFullName());
-            NameInformation ni = new NameInformation();
-            ni.setPersonalNameInformation(pni);
-            userOptionalFields.setNameInformation(ni);
-        }
+		try {
+			//Can restful apis authenticatE?
+			//user = alephRemoteServiceManager.authenticateUser(agencyId, patronId, password);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 
-        responseData.setUserOptionalFields(userOptionalFields);
+		// AgencyId agencyId = new AgencyId(dummySvcMgr.getLibraryName());
 
-        return responseData;
-    }
+		// Echo back the same item id that came in
+		responseData.setUserId(initData.getUserId());
+
+		UserOptionalFields userOptionalFields = new UserOptionalFields();
+
+		if (user != null && initData.getNameInformationDesired()) {
+			PersonalNameInformation pni = new PersonalNameInformation();
+			pni.setUnstructuredPersonalUserName(user.getFullName());
+			NameInformation ni = new NameInformation();
+			ni.setPersonalNameInformation(pni);
+			userOptionalFields.setNameInformation(ni);
+		}
+
+		responseData.setUserOptionalFields(userOptionalFields);
+
+		return responseData;
+	}
 
 }
