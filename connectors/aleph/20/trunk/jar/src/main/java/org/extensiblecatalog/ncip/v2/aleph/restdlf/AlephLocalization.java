50cec6ba65d225763e8e70f8a70ff12ff5a83a1b
Jiří Kozlovský <mail@jkozlovsky.cz>
2014-11-09 12:20:02 +0100

    [Aleph rest-dlf connector] Included omitted localization file from last commit

---
+++ connectors/aleph/20/trunk/jar/src/main/java/org/extensiblecatalog/ncip/v2/aleph/restdlf/AlephLocalization.java
@@ -0,0 +1,16 @@
+
+package org.extensiblecatalog.ncip.v2.aleph.restdlf;
+
+/**
+ * @author Jiří Kozlovský (MZK)
+ *
+ */
+public class AlephLocalization {
+	public static final String BOOK = "Kniha";
+	public static final String MAP = "Mapa";
+	public static final String GRAPHICS = "Grafika";
+	public static final String MAGAZINE1 = "Vazan. ser.";
+	public static final String MAGAZINE2 = "Seriál";
+	public static final String COMPACT_DISC = "Kompaktní disk";
+	public static final String AUDIO_TAPE = "Audiokazeta";	
+}
