070cc2682a7c9589038e6088c7fb91bb4989aa18
Jiří Kozlovský <mail@jkozlovsky.cz>
2014-11-09 17:04:54 +0100

    [Aleph tests] Removed hardly implementable prepared classes for tests

--- connectors/aleph/20/trunk/jar/src/test/java/org/extensiblecatalog/ncip/v2/aleph/AlephRenewItemTest.java
+++
@@ -1,9 +0,0 @@
-package org.extensiblecatalog.ncip.v2.aleph;
-
-import junit.framework.TestCase;
-
-public class AlephRenewItemTest extends TestCase {
-	public void testPerformService() {
-		
-	}
-}
