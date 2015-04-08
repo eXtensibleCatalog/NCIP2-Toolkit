e3d9d87e7089dc93db24bd11640ec46716a40b79
jirislav <mail@jkozlovsky.cz>
2015-04-08 19:23:02 +0200

    [Dockerfile] Included koha's connector to dockerfile's deployment instructions & fixed wrong aleph's version

--- dockerfile/xcncip2toolkit/maven_build_ncip2.sh
+++ dockerfile/xcncip2toolkit/maven_build_ncip2.sh
@@ -3,5 +3,39 @@ cd /home/xcncip2toolkit/xcncip2toolkit/core/trunk
 mvn install
 mvn install -Dmaven.test.skip
 
-cd ../../connectors/aleph/20/trunk
+cd ../../connectors/aleph/22/trunk
+mvn install -Dmaven.test.skip
+
+cd ../../../../connectors/koha/3.xx/trunk/web/src/main/resources
+
+config='toolkit.properties'
+cp toolkit.properties.example $config
+
+echo
+echo "Koha's configuration is on schedule.."
+echo ""
+read -p "Enter your library full address: " address
+echo ""
+read -p "Enter your library name: " libraryName
+echo ""
+read -p "Enter your SIGLA: " sigla
+echo ""
+read -p "Enter URL of your online registration form (optional): " registrationLink
+echo ""
+read -p "Enter IP address of your OPAC: " opac
+echo ""
+read -p "Enter Admin name to intranet: " adminName
+echo ""
+read -s -p "Enter Admin password: " adminPass
+
+sed -i "s_LibraryAddressHere_$(echo $address)_g" $config
+sed -i "s_LibraryNameHere_$(echo $libraryName)_g" $config
+sed -i "s_LibrarySIGLAHere_$(echo $sigla)_g" $config
+sed -i "s_LibraryRegistrationLinkHere_$(echo $registrationLink)_g" $config
+sed -i "s_KohaIPHere_$(echo $opac)_g" $config
+
+sed -i "s_IntranetAdministratorNameHere_$(echo $adminName)_g" $config
+sed -i "s_IntranetAdministratorPassHere_$(echo $adminPass)_g" $config
+
+cd ../../../..
 mvn install -Dmaven.test.skip
